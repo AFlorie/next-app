9e904c20da8da9ab6aaa66a051de80b7ab6202b0
Florie <florie.anstett@gmail.com>
2024-03-15 10:38:54 +0100

    contact style

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,5 +1,38 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const Contact = () => {
-  return <div>Contact page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src="/contact.png" alt="" fill />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name and Surname"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email address"
+            required
+          />
+          <input
+            type="phone"
+            name="phone"
+            placeholder="Phone number (optional)"
+          />
+          <textarea name="" id="" cols="30" rows="10" placeholder="Message" />
+
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
